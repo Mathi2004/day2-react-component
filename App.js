a9ce1c0a7e07240c3ed8e7f77cffbda6813efb19
SANMATHI L <110358404+Mathi2004@users.noreply.github.com>
2023-04-05 15:24:22 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Naving from './Naving';
+import Footer from './Footer';
+function App() {
+  return (
+    <div className="App">
+      <Naving/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
